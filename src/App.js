a6aadb79cebd807652048a5dbbc08b5d319862d1
biswalmanoj310 <manojbiswalcollege@gmail.com>
2024-05-12 09:52:26 -0700

    Added cart model

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
+// checking checking...App.apply.apply.apply       ÷
+
 function App() {
   return (
     <CartProvider>
